3688521f2f0550066d4f4eddadb120db330c1187
Wayne <wpa12@live.com>
2020-05-30 11:13:26 +0100

    Update app.js

--- javascript_version/app.js
+++ javascript_version/app.js
@@ -7,9 +7,6 @@
 //load in the lyricGeneratorClass
 import { LyricGeneratorClass } from './LyricGeneratorClass.js'; 
 
-
-
-
 // create an instance of LyricGenertorClass Class
 var lyrics = new LyricGeneratorClass(); 
 
@@ -26,6 +23,7 @@ let behaviour = document.querySelector('#behaviour');
 let action = document.querySelector('#action');
 let scene = document.querySelector('#scene');
 let contentContainer = document.querySelector('#lyricsContainer');
+
 // assign even listener for clicking the button
 genBtn.addEventListener('click', function(event){
 	event.preventDefault(); 
